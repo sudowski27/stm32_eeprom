25caf115a1b44cd4c73627dbd9d6b50bbd3f5cc7
sudowski27 <sudowski@o2.pl>
2022-12-11 16:51:27 +0100

    modyfikacja pliku 24LC01B.c

--- Src/24LC01B.c
+++ Src/24LC01B.c
@@ -23,22 +23,29 @@ void i2c_init() {
 	LL_GPIO_SetAFPin_0_7(GPIOB, LL_GPIO_PIN_8,  LL_GPIO_AF_4);
 
 	// Configure I2C
-	LL_I2C_InitTypeDef i2c_init;
-	LL_I2C_StructInit(&i2c_init);
+	LL_I2C_EnableReset(I2C1);
+	LL_I2C_DisableReset(I2C1);
+	//LL_I2C_InitTypeDef i2c_init;
+	//LL_I2C_StructInit(&i2c_init);
 
-	LL_I2C_Disable(I2C1);
 	LL_I2C_SetOwnAddress1(I2C1, 0x00, LL_I2C_OWNADDRESS1_7BIT);
 	LL_I2C_EnableClockStretching(I2C1);
-	LL_I2C_SetPeriphClock(I2C1, 42000000);
+	LL_I2C_DisableOwnAddress2(I2C1);
+	LL_I2C_DisableGeneralCall(I2C1);
+	//LL_I2C_SetPeriphClock(I2C1, 42000000);
 	LL_I2C_SetClockSpeedMode(I2C1, LL_I2C_CLOCK_SPEED_STANDARD_MODE);
+	//LL_I2C_DisableAnalogFilter
 	LL_I2C_ConfigSpeed(I2C1, 42000000, 100000, LL_I2C_DUTYCYCLE_2);
-
+	LL_I2C_AcknowledgeNextData(I2C1, LL_I2C_ACK);
 	LL_I2C_SetMode(I2C1, LL_I2C_MODE_I2C);
+
+	if(LL_I2C_IsResetEnabled(I2C1))
+		debug_diode_off();
 
 	LL_I2C_Enable(I2C1);
 
 	if(LL_I2C_IsEnabled(I2C1))
-		debug_diode_off();
+		debug_diode_on();
 }
 
 
@@ -63,28 +70,16 @@ void debug_diode_off(){
 
 void i2c_master_transmit(uint8_t slave_address, uint8_t *buffer, uint16_t size) {
 	// start condition
-	printf("LL_RCC_HSI_IsReady = %d\n", LL_RCC_HSI_IsReady());
-	printf("LL_RCC_HSI_GetCalibration = %d\n", LL_RCC_HSI_GetCalibration());
-	printf("%d\n", LL_I2C_GetPeriphClock(I2C1));
-	printf("LL_I2C_IsEnabled = %d\n", LL_I2C_IsEnabled(I2C1));
-	printf("LL_RCC_GetSysClkSource = %d\n", LL_RCC_GetSysClkSource());
-	printf("LL_RCC_SYS_CLKSOURCE_STATUS_PLL = %d\n", LL_RCC_SYS_CLKSOURCE_STATUS_PLL);
-	printf("LL_RCC_GetAHBPrescaler = %d\n", LL_RCC_GetAHBPrescaler());
-	printf("LL_RCC_GetAPB1Prescaler = %d\n", LL_RCC_GetAPB1Prescaler());
-	printf("LL_RCC_APB1_DIV_2 = %d\n", LL_RCC_APB1_DIV_2);
-	printf("LL_RCC_PLL_GetN = %d\n", LL_RCC_PLL_GetN());
-	LL_I2C_Enable(I2C1);
+	//debug_diode_on();
+	printf("\n\n\n\n");
 	LL_I2C_GenerateStartCondition(I2C1);
-	//debug_diode_on();
+
 	// Indicate the status of Start Bit
-	//printf("%d\n", LL_I2C_IsActiveFlag_SB(I2C1));
 	//while(!LL_I2C_IsActiveFlag_SB(I2C1)) {} // when is True escape from this loop
-	//printf("po while %d\n", LL_I2C_IsActiveFlag_SB(I2C1));
-	//debug_diode_on();
 	LL_I2C_TransmitData8(I2C1, slave_address); // send address of slave
-	printf("LL_I2C_IsActiveFlag_ADDR  = %d\n", LL_I2C_IsActiveFlag_ADDR(I2C1));
 
 	while(!LL_I2C_IsActiveFlag_ADDR(I2C1)) {} // indicate the status of address sent
+	printf("address send\n");
 
 	LL_I2C_ClearFlag_ADDR(I2C1); // clear address matched flag
 
