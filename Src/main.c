4b61612b17b23017a05caa4fb01d5baae605a63d
sudowski27 <sudowski@o2.pl>
2022-12-11 11:54:14 +0100

    Include string

--- Src/main.c
+++ Src/main.c
@@ -23,6 +23,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "24LC01B.h"
+#include <string.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
