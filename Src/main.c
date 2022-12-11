c060e093dd5c2e8804c2e9b4563763d29f2b6de1
sudowski27 <sudowski@o2.pl>
2022-12-11 17:31:34 +0100

    Wysłanie ramki do zapisu danych. Wysłanie adresu gdzie ma zostać zapisana dana i sama dana

--- Src/main.c
+++ Src/main.c
@@ -132,6 +132,7 @@ int main(void)
     /* USER CODE BEGIN 3 */
       buf[0] = 1;
       buf[1] = 123;
+      ret = HAL_I2C_Master_Transmit(&hi2c1, 0xA0, buf, 2, HAL_MAX_DELAY);
   }
   /* USER CODE END 3 */
 }
