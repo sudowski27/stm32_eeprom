639be8fd4d0d96985501e2a0ae11a42d1874f28b
sudowski27 <sudowski@o2.pl>
2022-12-11 17:08:01 +0100

    Dodanie zmiennej typu uint8_t jako bufor na dane wejściowe dla pamięci EEPROM

--- Src/main.c
+++ Src/main.c
@@ -86,7 +86,7 @@ void send_char(char c)
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+	uint8_t buf[12];
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
